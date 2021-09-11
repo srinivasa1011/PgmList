e1d14d2672b706382bf5e614c9e61e8f68728f9c
srinivasakeshakumar <52126869+srinivasakeshakumar@users.noreply.github.com>
2021-09-11 12:37:52 +0530

    Update B.java

--- src/main/java/JavaProg/Javaprogram/B.java
+++ src/main/java/JavaProg/Javaprogram/B.java
@@ -3,16 +3,4 @@ package JavaProg.Javaprogram;
 
 public class B {
 
-	public B() {
-		// TODO Auto-generated constructor stub
-	}
-=======
-Class B
-{
- public static void main(String[] args)
- {
-   System.out.println("hello");
- }
->>>>>>> bbea25ea79929563579437ed49a363d25ace380a
-
 }
