be1bfbcd52f6a65d4c5facb696ddac809f2188a3
KeshavKumar <srinivasa1011@gmail.com>
2021-09-11 12:09:18 +0530

    pgm;ist

---
+++ src/main/java/JavaProg/Javaprogram/App.java
@@ -0,0 +1,13 @@
+package JavaProg.Javaprogram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/JavaProg/Javaprogram/Java1.java
@@ -0,0 +1,15 @@
+package JavaProg.Javaprogram;
+
+public class Java1 {
+
+	public Java1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hi");
+
+	}
+
+}
